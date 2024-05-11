ad8d40c2b0b0bdc7dd33a32aa63a336ce9f13abc
Isarok <isaac.lucerito@gmail.com>
2024-05-11 14:30:18 +0200

    backend_feature/advance_addedEnvironmentVariables

--- Server/database/Config.ts
+++ Server/database/Config.ts
@@ -1,13 +1,18 @@
-import { createPool } from 'mysql2/promise';
+import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+dotenv.config();
 
 // Objeto CONFIG: contiene la configuración necesaria para establecer la conexión a la base de datos
-const CONFIG = {
-    host: 'localhost',
-    user: 'root',
-    password: '123456789',
-    database: 'apolliculture',
-    port: 3306
+  const {BD_HOSTNAME, BD_USERNAME, BD_PASSWORD, BD_NAME} = process.env;
+
+const config = {
+   host:  BD_HOSTNAME,
+    user: BD_USERNAME,
+    password: BD_PASSWORD,
+    database: BD_NAME   
 };
 
-export const connection = createPool(CONFIG);
+
+
+export const connection = mysql.createPool(config);
 
