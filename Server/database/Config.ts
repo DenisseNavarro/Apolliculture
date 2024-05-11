897ff9427c444c30f0a6966d36cca62c784def43
Isarok <isaac.lucerito@gmail.com>
2024-05-11 15:17:28 +0200

    back_feature/advance_addedEnviromentVariablePORT

--- Server/database/Config.ts
+++ Server/database/Config.ts
@@ -3,13 +3,14 @@ import mysql from 'mysql2/promise';
 dotenv.config();
 
 // Objeto CONFIG: contiene la configuración necesaria para establecer la conexión a la base de datos
-  const {BD_HOSTNAME, BD_USERNAME, BD_PASSWORD, BD_NAME} = process.env;
+  const {BD_HOSTNAME, BD_USERNAME, BD_PASSWORD, BD_NAME, PORT} = process.env;
 
 const config = {
    host:  BD_HOSTNAME,
     user: BD_USERNAME,
     password: BD_PASSWORD,
-    database: BD_NAME   
+    database: BD_NAME,
+    port: parseInt(PORT || '3306')  
 };
 
 
